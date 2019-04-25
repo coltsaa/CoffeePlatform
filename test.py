2194921be5301a78832371dd8c8195e81cb3b6a0
EiJi <ostantares@gmail.com>
2019-04-25 19:51:12 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author  : EiJi
+# @Email   : ostantares@gmail.com
+import os
+import os.path
+
+def dfs_showdir(path, depth):
+    if depth == 0:
+        print("root:[" + path + "]")
+
+    for item in os.listdir(path):
+        if '.git' not in item:
+            print("|      " * depth + "+--" + item)
+
+            newitem = path +'/'+ item
+            if os.path.isdir(newitem):
+                dfs_showdir(newitem, depth +1)
+
+if __name__ == '__main__':
+    dfs_showdir('.', 0)
